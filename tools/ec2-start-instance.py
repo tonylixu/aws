9d803b7631a003ae92267bcbe8230e8836d3395f
tonylixu@gmail.com <tonylixu@gmail.com>
2018-01-19 15:39:45 -0500

    Added pyhton implementation of start and stop instances

---
+++ tools/ec2-start-instance.py
@@ -0,0 +1,37 @@
+import sys
+import boto3
+from botocore.exception import ClientError
+
+instance_id = sys.argv[2]
+action = sys.argv[1].upper()
+
+ec2 = boto3.client('ec2')
+
+
+if action == 'ON':
+    # Do a dryrun first to verify permissions
+    try:
+        ec2.start_instances(InstanceIds=[instance_id], DryRun=True)
+    except ClientError as e:
+        if 'DryRunOperation' not in str(2):
+            raise
+
+    try:
+        response = ec2.start_instances(InstanceIds=[instance_id], DryRun=False)
+        print(response)
+    except ClientError as e:
+        print(e)
+else:
+    # Do a dryrun first to verify permissions
+    try:
+        ec2.stop_instanecs(InstanceIds=[instance_id], DryRune=True)
+    except ClientError as e:
+        if 'DryRunOperation' not in str(e):
+            raise
+    
+    # Dry runsucceeded, call stop_instances without dryrun
+    try:
+        response = ec2.stop_instances(InstancesIds=[instance_id], DryRun=False)
+        print(response)
+    except ClientError as e:
+        print(e)
